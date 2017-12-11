b725a09231dac45b425724de9e8a68f43c8323f0
therealtimsanchez55 <therealtimsanchez55@gmail.com>
2017-12-10 20:35:06 -0600

    Final changes

--- FSPACERFINAL/Startup.cs
+++ FSPACERFINAL/Startup.cs
@@ -26,8 +26,11 @@ namespace FSPACERFINAL
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("AzureConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
