d4be197c9136e82f3a4c51af0bda21e4c3d55abc
Timothy Sanchez <ts976273@wtstudents.wtamu.edu>
2017-12-10 19:38:43 -0600

    Add project files.

---
+++ FSPACERFINAL/Models/ActiveSearchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSPACERFINAL.Models
+{
+    public class ActiveSearchModel
+    {
+        public bool SearchActiveCards { get; set; }
+    }
+}
---
+++ FSPACERFINAL/Models/SpacerSearchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSPACERFINAL.Models
+{
+    public class SpacerSearchModel
+    {
+        public string SearchDriveNumber { get; set; }
+        public int SearchOperatorID { get; set; }
+        public DateTime SearchDate { get; set; }
+    }
+}
